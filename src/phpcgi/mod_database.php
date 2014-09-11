486609fa3ab68bf61611107c207387ca343ec38f
Daniel Kuznetsov <afc2008@yandex.ru>
2014-09-11 20:53:00 +0400

    Added threads_get.php

--- src/phpcgi/mod_database.php
+++ src/phpcgi/mod_database.php
@@ -1,6 +1,6 @@
 <?
 // mod_database.php
-// database module for <name>board
+// database module for suhtlus
 // copyright brotheroftux 2014
 // this code is distributed under GPLv3 license
 
@@ -13,4 +13,6 @@ class mod_database
     public function db_init(){
         if(constant("DB_TYPE") == mysql){
             $dbReference = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+            return $dbReference;
+        }
 }
---
+++ src/phpcgi/threads_get.php
@@ -0,0 +1,21 @@
+<?
+// threads_get.php
+// Get <count> threads from specified board
+// copyright 2014 brotheroftux
+// this code is distributed under GPLv3 license
+
+require_once("config.php");
+
+$responseArray = array();
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($mysqli->connect_errno) {
+    die("Could not connect to MySQL.\n"); // TODO: encode error messages in JSON
+}
+
+$mysqli->query("set names 'utf8';");
+$mysqli->query("set character set 'utf8';");
+$res = $mysqli->query("select * from ".DB_TABLE_PREFIX.$_GET['board']." limit ".$_GET['offset'].", ".$_GET['count'].";");
+echo json_encode($res);
+mysqli->close();
+
+?>
