3a27d7064dc906e95a64c3286373ba20c9d06e47
Daniel Kuznetsov <afc2008@yandex.ru>
2014-09-13 20:19:22 +0400

    Fixed a bug in pic_bg.php

--- src/phpcgi/pic_bg.php
+++ src/phpcgi/pic_bg.php
@@ -12,10 +12,7 @@
     imagecopyresampled($pixel, $image, 0, 0, 0, 0, 1, 1, $width, $height); // using default PHP GD function to resize image to 1x1 pixel pic -- it will be the average colour of the image
     $rgb = imagecolorat($pixel, 0, 0);
     $color = imagecolorsforindex($pixel, $rgb);
-    $colors['red']  = ($rgb >> 16) & 0xFF;
-    $colors['green'] = ($rgb >> 8) & 0xFF;
-    $blue['blue']  =  $rgb & 0xFF;
-    $luminance = (0.2126*$colors['red'] + 0.7152*$colors['green'] + 0.0722*$colors['blue']); // luminance formula, God bless wikipedia
+    $luminance = (0.2126*$color['red'] + 0.7152*$color['green'] + 0.0722*$color['blue']); // luminance formula, God bless wikipedia
     // echo $luminance; // now only for test
     if ($luminance <= 55){
         echo json_encode(array( "response" => "dark" ));
